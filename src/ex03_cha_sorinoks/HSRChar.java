f8d7b92bed377b35e0636ffdd9e4fb4fe03aa4a8
KileySorino <112676590+KileySorino@users.noreply.github.com>
2023-09-15 20:21:09 +0800

    Create HSRChar.java

---
+++ src/ex03_cha_sorinoks/HSRChar.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ex03_cha_sorinoks;
+
+/**
+ *
+ * @author Kiley
+ */
+
+
+public class HSRChar{
+    private String name, location, path, combattype;
+    private int rarity;
+    
+    public HSRChar(String n, String l, int r, String p, String c){
+    name = n;
+    location = l;
+    rarity = r;
+    path = p;
+    combattype = c;
+    }
+}
