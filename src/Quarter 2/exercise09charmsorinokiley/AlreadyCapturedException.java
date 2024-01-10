8fe2bf7d37a8006375de989c86084aa853a8dd92
KileySorino <112676590+KileySorino@users.noreply.github.com>
2024-01-10 22:00:56 +0800

    Create AlreadyCapturedException.java

---
+++ src/Quarter 2/exercise09charmsorinokiley/AlreadyCapturedException.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exercise09charmsorinokiley;
+
+/**
+ *
+ * @author Kiley
+ */
+public class AlreadyCapturedException extends Exception {
+    public AlreadyCapturedException(){
+    }
+    
+    public AlreadyCapturedException(String msg){
+        super(msg);
+    }
+}
