024f0fa1eda40f422474c9b601a65086722b20a7
KileySorino <112676590+KileySorino@users.noreply.github.com>
2024-01-10 22:00:34 +0800

    Create MonsterNotFoundException.java

---
+++ src/Quarter 2/exercise09charmsorinokiley/MonsterNotFoundException.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exercise09charmsorinokiley;
+
+/**
+ *
+ * @author Kiley
+ */
+public class MonsterNotFoundException extends Exception {
+    public MonsterNotFoundException(){
+    }
+    
+    public MonsterNotFoundException(String msg){
+        super(msg);
+    }
+    
+
+}
+
