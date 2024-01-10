44b2641d759ec4a3cf516c1f19af6339d44ae02d
KileySorino <112676590+KileySorino@users.noreply.github.com>
2024-01-10 22:02:37 +0800

    Create NotInTeamException.java

---
+++ src/Quarter 2/exercise09charmsorinokiley/NotInTeamException.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exercise09charmsorinokiley;
+
+/**
+ *
+ * @author Kiley
+ */
+public class NotInTeamException extends Exception {
+    public NotInTeamException (){
+    }
+    
+    public NotInTeamException (String msg){
+        super(msg);
+    }
+}
