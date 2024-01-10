4cd6ba50fa63170edeb53da7740e2ebd5f32a0a3
KileySorino <112676590+KileySorino@users.noreply.github.com>
2024-01-10 22:02:04 +0800

    Create FullTeamException.java

---
+++ src/Quarter 2/exercise09charmsorinokiley/FullTeamException.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exercise09charmsorinokiley;
+
+/**
+ *
+ * @author Kiley
+ */
+public class FullTeamException extends Exception {
+    public FullTeamException(){
+    }
+    
+    public FullTeamException(String msg){
+        super(msg);
+    }
+}
